166bf455e7c6eda3617e83157c46103866126240
jrzkaminski <86363785+jrzkaminski@users.noreply.github.com>
2022-09-16 00:37:56 +0300

    SMM lab completed

--- cmm-labs/assignment_3/diffeq_script.py
+++ cmm-labs/assignment_3/diffeq_script.py
@@ -1,6 +1,7 @@
 from cmath import tan
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def Euler_method(interval: tuple, step: float):
@@ -11,17 +12,19 @@ def Euler_method(interval: tuple, step: float):
         y = y + step * drv(x, y)
         x = x + step
         y_arr.append(y)
-        print(x, y)
     return y_arr
+
 
 def Euler_Cauchy_method(interval: tuple, step: float):
     x = interval[0]
     y = interval[0]
+    y_arr = []
     while x < interval[1]:
-        y = y + step * drv(x, y)
+        y = y + (step * drv(x, y) + step * drv(x + step, y + step * drv(x, y))) / 2
         x = x + step
-        print(x, y)
-    return y
+        y_arr.append(y)
+    return y_arr
+
 
 def Runghe_Kutta_method(interval: tuple, step: float):
     x = interval[0]
@@ -35,14 +38,26 @@ def Runghe_Kutta_method(interval: tuple, step: float):
         y = y + step * (k1 + 2 * k2 + 2 * k3 + k4) / 6
         x = x + step
         y_arr.append(y)
-        print(x, y)
     return y_arr
 
-def Multistep_Adam_method(drv, interval, step):
-    pass
+
+def Multistep_Adams_method(interval: tuple, step: float):
+    x = interval[0] + step * 5
+    y_arr = Runghe_Kutta_method((0, 0.5), 0.1)
+    print(y_arr)
+    y = y_arr[-1]
+    while x < interval[1]:
+        y = y + step * (55 * drv(x, y) - 59 * drv(x - step, y - step * drv(x, y)) + 37 * drv(x - 2 * step,
+                        y - 2 * step * drv(x, y)) - 9 * drv(x - 3 * step, y - 3 * step * drv(x, y))) / 24
+        x = x + step
+        y_arr.append(y)
+    print(y_arr)
+    return y_arr
+
 
 def drv(x: float, y: float):
     return (y+x)**2
+
 
 def exact_func(interval: tuple, step: float):
     x = interval[0]
@@ -50,23 +65,29 @@ def exact_func(interval: tuple, step: float):
     while x < interval[1]:
         y = tan(x) - x
         x = x + step
-        y_arr.append(y)
-        print(x, y)
+        y_arr.append(y.real)
     return y_arr
+
 
 if __name__ == "__main__":
     y_euler = Euler_method((0, 1), 0.1)
     y_euler_cauchy = Euler_Cauchy_method((0, 1), 0.1)
     y_runghe_Kutta = Runghe_Kutta_method((0, 1), 0.1)
-    y_exact = exact_func((0, 1), 0.1)
+    y_multistep_adam = Multistep_Adams_method((0, 1), 0.1)
+    y_exact = exact_func((0.1, 1.1), 0.1)
 
-    x = np.arange(0, 1, 0.1)
+    x = np.arange(0, 1.01, 0.1)
 
-    print(x, y_euler)
+    df = pd.DataFrame({'x': x, 'Euler': y_euler, 'Euler-Cauchy': y_euler_cauchy,
+                      'Runghe-Kutta': y_runghe_Kutta, 'Multistep-Adam': y_multistep_adam, 'Exact': y_exact})
 
-    plt.scatter(x, y_euler, label="Euler")
-    plt.scatter(x, y_euler_cauchy, label="Euler-Cauchy")
-    plt.scatter(x, y_runghe_Kutta, label="Runghe-Kutta")
-    plt.scatter(x, y_exact, label="Exact")
+    print(df)
+
+    plt.plot(x, y_euler, label="Euler")
+    plt.plot(x, y_euler_cauchy, label="Euler-Cauchy")
+    plt.plot(x, y_runghe_Kutta, label="Runghe-Kutta")
+    plt.plot(x, y_multistep_adam, label="Multistep-Adam")
+    plt.plot(x, y_exact, label="Exact")
     plt.legend()
+    plt.grid()
     plt.savefig("diffeq.png")
